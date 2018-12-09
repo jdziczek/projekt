72e49613aca845853d9910c86a2af0eb874baa2f
bartlej <bjarzembinski@yahoo.pl>
2018-12-09 22:04:37 +0100

    many changes - code quality, improve subpages, remove node_modules

---
+++ webpage/components/zlecenie_dodaj.php
@@ -0,0 +1,44 @@
+<div class="modal fade" id="noweZlecenieModal" tabindex="-1" role="dialog" aria-labelledby="noweZlecenieModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+          <span class="sr-only">Close</span>
+        </button>
+        <h4 class="modal-title" id="noweZlecenieModalLabel">Dodawanie zlecenia</h4>
+      </div>
+      <div class="modal-body">
+        <div class="form">
+          <div>
+            <label>Wielkość auta:</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <label>Typ ładunku:</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <label>Liczba ludzi do załadunku i rozładunku:</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <label>Adres początkowy:</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <label>Adres docelowy:</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <button>Dodaj</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
---
+++ webpage/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <title>JBD Logistics</title>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue.css">
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <?php require_once "templates/sidebar_zaloguj.php"; ?>
+    <div class="w3-main">
+      <?php require_once "templates/header.php"; ?>
+      <div class="w3-container">
+      <!-- content -->
+        <h2>JBD Logistics Forwarder App</h2>
+        <p>
+          Nasza aplikacja to bardzo popularny, wszechstronny i nowoczesny program do obsługi spedycji krajowej. Posiadamy wieloletnie doświadczenie 
+          w projektowaniu i implementacji systemów dedykowanych spedycji i transportowi. Zachęcamy do korzystania z naszej aplikacji.
+        </p>
+        <img class="centered_image" src="assets/spedycja.jpg" style="width:40%" alt="spedycja">
+      <!-- content -->
+      </div>
+      <?php require_once "templates/footer.php"; ?>
+    </div>
+    <script src="/js/main.js"></script>
+  </body>
+</html>
