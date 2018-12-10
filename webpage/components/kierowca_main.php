839b7878172d3cb79b09e5319c85a0384378b35c
bartlej <bjarzembinski@yahoo.pl>
2018-12-10 23:44:36 +0100

    kierowca bugfix

--- webpage/components/kierowca_main.php
+++ webpage/components/kierowca_main.php
@@ -99,7 +99,7 @@
       </div>
       <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php'; ?>
     </div>
-    <?php //require_once $_SERVER['DOCUMENT_ROOT'] . '/components/zlecenie_szczegoly_script.php'; ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/components/zlecenie_szczegoly.php'; ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="/js/main.js"></script>
