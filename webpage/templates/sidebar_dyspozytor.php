b4e780f212a43497bb059cdb1d7f4ccae795d30b
GER\bjarzemb <bartlomiej.jarzembinski@intel.com>
2019-01-15 14:33:16 +0100

    php v2

--- webpage/templates/sidebar_dyspozytor.php
+++ webpage/templates/sidebar_dyspozytor.php
@@ -1,6 +1,6 @@
     <nav class="w3-sidebar w3-bar-block w3-collapse w3-animate-left w3-card" id="sidebar">
-      <a class="w3-bar-item w3-button w3-blue" href="/components/dyspozytor_main">Strona główna</a>
-      <a class="w3-bar-item w3-button " href="/components/calculator">Kalkulator</a>
+      <a class="w3-bar-item w3-button w3-blue" href="/components/dyspozytor_main.php">Strona główna</a>
+      <a class="w3-bar-item w3-button " href="/components/calculator.php">Kalkulator</a>
       <a class="w3-bar-item w3-button " href="/components/logout.php">Wyloguj się</a>
 	  <a class="w3-bar-item w3-button " href="/components/haslo.php">Zmień hasło</a>
       <a class="w3-bar-item w3-button w3-large w3-hide-large center_button" href="javascript:void(0)" onclick="hideSideBar()"><i class="fa fa-remove" id="remove_icon"></i></a>
