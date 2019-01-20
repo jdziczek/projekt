c005be502de4666adc97e523da526e0aa95b9980
bartlej <bjarzembinski@yahoo.pl>
2019-01-20 01:28:10 +0100

    add more info to dispatcher view and improve layout

--- webpage/templates/sidebar_kierowca.php
+++ webpage/templates/sidebar_kierowca.php
@@ -2,7 +2,7 @@
       <a class="w3-bar-item w3-button w3-blue" href="/components/kierowca_main.php">Strona główna</a>
       <a class="w3-bar-item w3-button " href="/components/logout.php">Wyloguj się</a>
 	  <a class="w3-bar-item w3-button " href="/components/haslo.php">Zmień hasło</a>
-      <a class="w3-bar-item w3-button w3-large w3-hide-large center_button" href="javascript:void(0)" onclick="hideSideBar()"><i class="fa fa-remove" id="remove_icon"></i></a>
+      <a class="w3-bar-item w3-button w3-large w3-hide-large center" href="javascript:void(0)" onclick="hideSideBar()"><i class="fa fa-remove" id="remove_icon"></i></a>
     </nav>
     <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="hideSideBar()" style="cursor:pointer" id="shadow">
     </div>
