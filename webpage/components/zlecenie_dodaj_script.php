6ea9911256b6d40cde4ea3e0cd078a6726080690
bartlej <bjarzembinski@yahoo.pl>
2019-01-22 13:18:43 +0100

    stored procedures

--- webpage/components/zlecenie_dodaj_script.php
+++ webpage/components/zlecenie_dodaj_script.php
@@ -1,38 +1,44 @@
 <?php
-	
-	session_start();
+
+  session_start();
+
 // polaczenie z baza
   require_once "connect.php";
 	$conn = mysqli_connect($host, $db_user, $db_password, $db_name);
 
-  // insert adresu poczatkowego
-  $sql = "INSERT INTO address (street, b_number, a_number, zip_code, city, floor)
-  VALUES ('".$_POST['f_adress_street']."', '".$_POST['f_adress_b_number']."', '".$_POST['f_adress_a_number']."', '".$_POST['f_adress_zip_code']."', '".$_POST['f_adress_city']."', '".$_POST['f_adress_floor']."')";
-  
-  $result = mysqli_query($conn, $sql);
-  $f_adress_id = $conn->insert_id;
 
-  // insert adresu koncowego
-  $sql = "INSERT INTO address (street, b_number, a_number, zip_code, city, floor)
-  VALUES ('".$_POST['s_adress_street']."', '".$_POST['s_adress_b_number']."', '".$_POST['s_adress_a_number']."', '".$_POST['s_adress_zip_code']."', '".$_POST['s_adress_city']."', '".$_POST['s_adress_floor']."')";
-  
+  // insert adresu poczatkowego
+  $sql = "CALL DodajAdres('".$_POST['f_adress_street']."', '".$_POST['f_adress_b_number']."', '".$_POST['f_adress_a_number']."', '".$_POST['f_adress_zip_code']."', '".$_POST['f_adress_city']."', '".$_POST['f_adress_floor']."')";  
   $result = mysqli_query($conn, $sql);
-  $s_adress_id = $conn->insert_id;
+
+  $sql = "SELECT MAX(id_address) FROM address";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $f_adress_id = $row['MAX(id_address)'];
+
+  // $call = "CALL id_adresu";
+  // $res = mysqli_query($conn, $call); 
+  // $row = mysqli_fetch_array($res);
+  // $f_adress_id = $row['MAX(id_address)'];
+
+   // insert adresu koncowego
+  $sql = "CALL DodajAdres2('".$_POST['s_adress_street']."', '".$_POST['s_adress_b_number']."', '".$_POST['s_adress_a_number']."', '".$_POST['s_adress_zip_code']."', '".$_POST['s_adress_city']."', '".$_POST['s_adress_floor']."')";
+  $result = mysqli_query($conn, $sql); 
+  $s_adress_id = $f_adress_id+1; 
+
 
   // insert ladunku
-  $sql = "INSERT INTO cargo (cargo_dsc, cargo_weight, cargo_length, cargo_width, cargo_height)
-  VALUES ('".$_POST['cargo_dsc']."', '".$_POST['cargo_weight']."', '".$_POST['cargo_length']."', '".$_POST['cargo_width']."', '".$_POST['cargo_height']."')";
-  
+  $sql = "CALL DodajLadunek('".$_POST['cargo_dsc']."', '".$_POST['cargo_weight']."', '".$_POST['cargo_length']."', '".$_POST['cargo_width']."', '".$_POST['cargo_height']."')";
   $result = mysqli_query($conn, $sql);
-  $id_cargo = $conn->insert_id;
+  $id_cargo = ($s_adress_id/2);
+
 
   $id_dispatcher=$_SESSION['id_uzytkownika'];
 
   //insert zamowienia
   $mysqldate = date("Y-m-d");
   $mysqltime = date("H:i:s");
-  $sql = "INSERT INTO orders (order_date, order_time, car_type, people, f_address, s_address, phone, distance, valuation, id_cargo, id_crew, status, id_dispatcher, comment_disp, comment_driver)
-  VALUES ('$mysqldate', '$mysqltime', '".$_POST['car_type']."', '".$_POST['people']."', $f_adress_id, $s_adress_id, '".$_POST['phone']."', '".$_POST['distance']."', '".$_POST['valuation']."', $id_cargo, '".$_POST['id_crew']."', 'przyjete', $id_dispatcher, '".$_POST['comment_dispatcher']."', '".$_POST['comment_driver']."')";
+  $sql = "CALL DodajZlecenie('$mysqldate', '$mysqltime', '".$_POST['car_type']."', '".$_POST['people']."', $f_adress_id, $s_adress_id, '".$_POST['phone']."', '".$_POST['distance']."', '".$_POST['valuation']."', $id_cargo, '".$_POST['id_crew']."', 'przyjete', $id_dispatcher, '".$_POST['comment_dispatcher']."', '".$_POST['comment_driver']."')";
 
   if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
